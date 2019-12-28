0a6eba39bbcce593e1e4515e965b709400aae21c
araji <abdelaziz.raji@gmail.com>
2019-12-28 00:53:37 -0500

    minor fixes, tested against reeference implemntation and simulator

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -40,7 +40,7 @@ class Controller(object):
             self.throttle_controller.reset()
             return 0., 0., 0.
 
-        current_vel = self.vel_lpf.filr(current_vel)
+        current_vel = self.vel_lpf.filt(current_vel)
         steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
         
         vel_error = linear_vel - current_vel
@@ -53,9 +53,10 @@ class Controller(object):
         throttle = self.throttle_controller.step(vel_error, sample_time)
         brake = 0
 
-        if linear_vel ==0 and current_vel < 0.1:
+        if linear_vel == 0. and current_vel < 0.1:
             throttle = 0
             brake = 400 
+
         elif throttle< .1 and vel_error <0:
             throttle = 0
             decel = max(vel_error, self.decel_limit)
